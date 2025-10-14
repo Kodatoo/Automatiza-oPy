6367e670b662f10693e4cad314afbb38972e6308
Kodatoo <gkodatofaria@gmail.com>
2025-10-14 14:18:50 -0300

    feat: Adição dos arquivos main, gitignore e requirements.txt

---
+++ src/main.py
@@ -0,0 +1,16 @@
+# Bibliotecas padrão do Python
+import os
+import time
+import datetime
+import shutil
+import glob
+
+# Selenium e WebDriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Manipulação de Excel
+import openpyxl
